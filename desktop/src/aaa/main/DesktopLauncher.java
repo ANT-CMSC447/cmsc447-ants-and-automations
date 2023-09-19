a49b7bb924a4193bdc01f324eed4fc2636a72e24
Jane Coomber <jcoombe1@umbc.edu>
2023-09-19 18:01:48 -0400

    default libgdx template

---
+++ desktop/src/aaa/main/DesktopLauncher.java
@@ -0,0 +1,14 @@
+package aaa.main;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("Ant Simulator");
+		new Lwjgl3Application(new AntGame(), config);
+	}
+}
