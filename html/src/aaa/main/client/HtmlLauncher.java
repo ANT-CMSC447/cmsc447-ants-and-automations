1dc0b2e0f29a0519ab9f82232b3157e5444fb0ed
Jane Shimmer <jane@j4.pm>
2023-09-30 18:40:00 -0400

    ANT-7: (chore) add in html backend

---
+++ html/src/aaa/main/client/HtmlLauncher.java
@@ -0,0 +1,22 @@
+package aaa.main.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import aaa.main.AntGame;
+
+public class HtmlLauncher extends GwtApplication {
+
+    @Override
+    public GwtApplicationConfiguration getConfig () {
+        // Resizable application, uses available space in browser
+        return new GwtApplicationConfiguration(true);
+        // Fixed size application:
+        //return new GwtApplicationConfiguration(480, 320);
+    }
+
+    @Override
+    public ApplicationListener createApplicationListener () {
+        return new AntGame();
+    }
+}
