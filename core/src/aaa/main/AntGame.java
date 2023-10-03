0735da25c1885153371aea85a591fbe7533cfa13
Jane Coomber <jcoombe1@umbc.edu>
2023-10-03 18:42:13 -0400

    (feat): add main menu screen, incl. some stubs for pause menu

--- core/src/aaa/main/AntGame.java
+++ core/src/aaa/main/AntGame.java
@@ -1,30 +1,52 @@
 package aaa.main;
 
+import aaa.main.game.GameState;
 import aaa.main.screens.MainScreen;
 import aaa.main.screens.MenuScreen;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class AntGame extends Game {
 	public SpriteBatch batch;
 	public ShapeRenderer shapeRenderer;
 	public BitmapFont font;
+
+	public GameState gameState;
+
+	public TextButton.TextButtonStyle buttonStyle;
 	@Override
 	public void create () {
 //		Gdx.graphics.setFullscreenMode(Gdx.graphics.getDisplayMode()); // enable fullscreen ?
+		gameState = loadState();
 		batch = new SpriteBatch();
 		shapeRenderer = new ShapeRenderer();
 		font = new BitmapFont();
+		buttonStyle = new TextButton.TextButtonStyle();
+		buttonStyle.font = this.font;
+		buttonStyle.fontColor = Color.RED;
 		setScreen(new MenuScreen(this));
+	}
+
+	public GameState loadState() {
+		// eventually handle save state loading here
+		return new GameState();
 	}
 	
 	@Override
 	public void dispose () {
 		super.dispose();
+	}
+
+	public void quit() {
+		this.pause();
+		this.dispose();
+		System.exit(0);
 	}
 }
--- core/src/aaa/main/screens/MainScreen.java
+++ core/src/aaa/main/screens/MainScreen.java
@@ -1,61 +1,79 @@
 package aaa.main.screens;
 
 import aaa.main.AntGame;
+import aaa.main.stages.PauseMenu;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.InputAdapter;
-import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.utils.Scaling;
-import com.badlogic.gdx.utils.viewport.ScalingViewport;
-import com.badlogic.gdx.utils.viewport.Viewport;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
 
 public class MainScreen extends ScreenAdapter {
     public static final int SCREEN_WIDTH = 1080;
     public static final int SCREEN_HEIGHT = 720;
-//    OrthographicCamera camera;
-//    Viewport viewport;
 
-    private AntGame game;
-    private Texture img;
+    private final AntGame game;
+    private final PauseMenu pauseMenu;
+    private final Stage stage;
+    private final Texture tex;
 
-    public MainScreen(AntGame game) {
+    private boolean pauseOpened = false;
+
+    public MainScreen(final AntGame game) {
         this.game = game;
-        img = new Texture("ant.png");
-    }
-    @Override
-    public void show() {
-        Gdx.input.setInputProcessor(new InputAdapter() {
+        tex = new Texture("ant.png");
+        stage = new Stage();
+
+        Label.LabelStyle labelStyle = new Label.LabelStyle();
+        labelStyle.fontColor = Color.BLUE;
+        labelStyle.font = game.font;
+        Label mainText = new Label("Main Screen", labelStyle);
+        mainText.setPosition(450f, 400f);
+        
+        Image image = new Image(tex);
+        image.setSize(300f, 300f);
+        
+        stage.addActor(mainText);
+        stage.addActor(image);
+
+        stage.addListener(new InputListener() {
             @Override
-            public boolean keyDown(int keycode) {
+            public boolean keyDown(InputEvent ev, int keycode) {
                 if (keycode == com.badlogic.gdx.Input.Keys.ESCAPE) {
-                    //show pause menu
-                    //game.setScreen(new MenuScreen(game));
-                }
-                if (keycode == com.badlogic.gdx.Input.Keys.SPACE) {
-                    game.setScreen(new MenuScreen(game));
+                    game.gameState.paused = true;
                 }
                 return true;
             }
         });
-    }
 
-//    public void initializeViewport() {
-//        camera = new OrthographicCamera();
-//        viewport = new ScalingViewport(Scaling.fit, SCREEN_WIDTH, SCREEN_HEIGHT, camera);
-//    }
+        pauseMenu = new PauseMenu(game);
+    }
+    @Override
+    public void show() {
+        Gdx.input.setInputProcessor(stage);
+    }
 
     @Override
     public void render(float delta) {
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        if (game.gameState.paused && !pauseOpened) {
+            Gdx.input.setInputProcessor(pauseMenu.getStage());
+            pauseOpened = true;
+        } else if (!game.gameState.paused && pauseOpened) {
+            Gdx.input.setInputProcessor(stage);
+            pauseOpened = false;
+        }
         game.batch.begin();
-        game.font.setColor(Color.RED);
-        game.batch.draw(img, 0, 0, 300, 300);
-        game.font.draw(game.batch, "Main Game", 310, 310);
+        stage.draw();
+        if (game.gameState.paused) {
+            this.pauseMenu.getStage().draw();
+        }
         game.batch.end();
     }
 
@@ -81,6 +99,6 @@ public class MainScreen extends ScreenAdapter {
 
     @Override
     public void dispose() {
-        img.dispose();
+        tex.dispose();
     }
 }
