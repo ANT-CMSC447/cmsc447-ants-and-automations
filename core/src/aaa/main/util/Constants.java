187425d78b44ae837d4a4ad2f8d340b6c2134018
gsingh2124 <gsingh10@umbc.edu>
2023-10-05 15:06:50 -0400

    Changed ant.png to a better ant for future purposes. Added Constants.java to better keep constants for future development.
    Added a world to MainScreen.java. This world is where our game will play out. So far, there is a box (world border, size adjustable via Constants.java) that contains 4 boxes (acting as up, down, left, right walls for the border).
    
    There is also a box for the player. The player box is able to move around with WASD or arrow keys. The player is able to not pass through the border box.
    
    The player also has a camera attached to always keep the player centred.
    
    There are a variety of movement methods we can use. Checkout MainScreen.inputUpdate to modify CameraChoice to test different movement mechanics to see which one we should use. (I personally vote physics without momentum).
    
    Also removed Image actor, not needed AFAIK.

---
+++ core/src/aaa/main/util/Constants.java
@@ -0,0 +1,11 @@
+package aaa.main.util;
+
+public final class Constants {
+    //PPM = Pixels Per Meter, since Box2D uses meters, and we use pixels (for scaling)
+    public static final float PPM = 32;
+    public static final int BORDER_WIDTH = 200;
+    public static final int BORDER_HEIGHT = 200;
+
+    public static final int SCREEN_WIDTH = 1080;
+    public static final int SCREEN_HEIGHT = 720;
+}
